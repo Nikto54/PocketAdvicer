f439719ebad27014921307f7aa668e6ebb358057
Богдан ебик <bondarbm03@gmail.com>
2023-12-10 22:45:34 +0300

    НИХУЯ НЕ РАБОТАЕТ

--- database.py
+++ database.py
@@ -1,17 +1,63 @@
 import sqlite3
+from random import random
+
 
 def create_connection():
     return sqlite3.connect('my_database.db')
 
+def find_user(tg_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE tg_id = ?', (tg_id,))
+    if cursor.fetchone():
+        return True
+    conn.close()
+
+
+
+def get_random_year_for_user(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT year_start, year_end FROM Years WHERE user_id = ?', (user_id,))
+    intervals = cursor.fetchall()
+
+    conn.close()
+
+    if not intervals:
+        return None
+
+    random_interval = random.choice(intervals)
+
+    random_year = random.randint(random_interval[0], random_interval[1])
+
+    return random_year
+
+def get_random_country_for_user(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    query = """
+    SELECT name FROM Countries 
+    WHERE user_id = ? 
+    ORDER BY RANDOM() 
+    LIMIT 1
+    """
+    cursor.execute(query, (user_id,))
+
+    result = cursor.fetchone()
+
+    if result:
+        return result[0]
+    conn.close()
 
 def add_user(tg_id):
     conn = create_connection()
     cursor = conn.cursor()
 
-    cursor.execute('SELECT COUNT(*) FROM Users WHERE tg_id = ?', (tg_id,))
-    count = cursor.fetchone()[0]
+    cursor.execute('SELECT * FROM Users WHERE tg_id = ?', (tg_id,))
+    count = cursor.fetchone()
 
-    if count == 0:
+    if not count:
         cursor.execute('INSERT INTO Users(tg_id) VALUES (?)', (tg_id,))
         conn.commit()
 
@@ -22,10 +68,10 @@ def add_country(tg_id, country_name):
     conn = create_connection()
     cursor = conn.cursor()
 
-    cursor.execute('SELECT COUNT(*) FROM Countries WHERE user_id = ? AND name = ?', (tg_id, country_name))
-    count = cursor.fetchone()[0]
+    cursor.execute('SELECT * FROM Countries WHERE user_id = ? AND name = ?', (tg_id, country_name))
+    count = cursor.fetchone()
 
-    if count == 0:
+    if not count == 0:
         cursor.execute('INSERT INTO Countries(user_id, name) VALUES (?, ?)', (tg_id, country_name))
         conn.commit()
 
@@ -36,12 +82,12 @@ def add_time_interval(user_id, year_start, year_end):
     conn = create_connection()
     cursor = conn.cursor()
 
-    cursor.execute('SELECT COUNT(*) FROM Years WHERE user_id = ? AND year_start = ? AND year_end = ?',
+    cursor.execute('SELECT * FROM Years WHERE user_id = ? AND year_start = ? AND year_end = ?',
                    (user_id, year_start, year_end))
-    count = cursor.fetchone()[0]
+    count = cursor.fetchone()
 
 
-    if count == 0:
+    if not count :
         cursor.execute('INSERT INTO Years(user_id, year_start, year_end) VALUES (?, ?, ?)',
                        (user_id, year_start, year_end))
         conn.commit()
@@ -50,7 +96,12 @@ def add_time_interval(user_id, year_start, year_end):
 connection = create_connection()
 
 cursor = connection.cursor()
-
+cursor.execute('''
+    DROP TABLE IF EXISTS Users ''')
+cursor.execute('''
+    DROP TABLE IF EXISTS Countries ''')
+cursor.execute('''
+    DROP TABLE IF EXISTS Years ''')
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Users (
      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,                                
@@ -73,9 +124,11 @@ cursor.execute('''
     user_id INTEGER,
     year_start INTEGER UNIQUE NOT NULL,
     year_end INTEGER UNIQUE NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES Users(tg_id)
+    FOREIGN KEY(user_id) REFERENCES Users(tg_id) 
 )
+
 ''')
+
 
 connection.commit()
 connection.close()
--- functions.py
+++ functions.py
@@ -9,6 +9,8 @@ import soundfile as sf
 from fer import FER
 from matplotlib import pyplot as plt
 from telebot import types
+from database import (add_user, add_country, add_time_interval, get_random_country_for_user,get_random_year_for_user
+,find_user)
 
 API_LINK = 'https://api.kinopoisk.dev/v1.4/movie?genres.name={genres_name}&page={count}'
 
@@ -31,7 +33,7 @@ TRANSLATE_DICT_TEXT = {
     'disgust': 'отвращение',
 
 }
-
+print(API_LINK)
 GENRE_DICT = {
     'anger': 'драма',
     'enthusiasm': 'фантастика',
@@ -58,24 +60,35 @@ def ask_for_another_file(message, bot):
 
 def process_text_message(message, bot):
     try:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Мои предпочтения')
+        item2 = types.KeyboardButton('Случайный фильм')
+        markup.add(item1, item2)
         text = message.text
         model = HuggingFaceModel.Text.Bert_Tiny2
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         tr = TextRecognizer(model=model, device=device)
         emot = tr.recognize(text, return_single_label=True)
-        bot.send_message(message.chat.id, f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_TEXT.get(emot, emot)}')
-        message = bot.send_message(message.chat.id, f'Начинается поиск фильма для вас\nПодождите пару секунд')
-        found_film(message, bot, emot)
-        ask_for_another_file(message, bot)
+        bot.send_message(message.chat.id, f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_TEXT.get(emot, emot)}',
+                         reply_markup=types.ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, f'Хотите выбрать случайный фильм или по вашим предпочтениям?',
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, extends_found_film, bot, emot)
+
 
 
     except Exception as e:
         bot.send_message(message.chat.id, 'Вы ввели не тот формат,что указали выше, Введите текстовое сообщение:')
+        print(e)
         bot.register_next_step_handler(message, process_text_message, bot)
 
 
 def process_audio_message(message, bot):
     try:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Мои предпочтения')
+        item2 = types.KeyboardButton('Случайный фильм')
+        markup.add(item1, item2)
         file_info = bot.get_file(message.voice.file_id)
         file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(
             '6317401550:AAG1C'
@@ -90,13 +103,13 @@ def process_audio_message(message, bot):
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         vr = VoiceRecognizer(model=model, device=device)
         emot = vr.recognize('voice.wav', return_single_label=True)
-        bot.send_message(message.chat.id, f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_TEXT.get(emot, emot)}')
-        message = bot.send_message(message.chat.id, f'Начинается поиск фильма для вас\nПодождите пару секунд')
-        found_film(message, bot, emot)
         os.remove('voice.wav')
         os.remove('voice.ogg')
-        ask_for_another_file(message, bot)
-
+        bot.send_message(message.chat.id, f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_TEXT.get(emot, emot)}',
+                         reply_markup=types.ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, f'Хотите выбрать случайный фильм или по вашим предпочтениям?',
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, extends_found_film, bot, emot)
 
     except Exception as e:
         bot.send_message(message.chat.id, 'Вы ввели не тот формат что указали выше!')
@@ -105,6 +118,9 @@ def process_audio_message(message, bot):
 
 def process_photo_message(message, bot):
     try:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Мои предпочтения')
+        item2 = types.KeyboardButton('Случайный фильм')
         photo = message.photo[-1]
         file_id = photo.file_id
         file_info = bot.get_file(file_id)
@@ -115,12 +131,14 @@ def process_photo_message(message, bot):
             photo_file.write(downloaded_file)
         test_image_one = plt.imread("photo.jpg")
         dominant_emotion, emotion_score = detector.top_emotion(test_image_one)
-        bot.send_message(message.chat.id, f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_FER.get(dominant_emotion, dominant_emotion)}')
-        message = bot.send_message(message.chat.id, f'Начинается поиск фильма для вас\nПодождите пару секунд')
+        bot.send_message(message.chat.id,
+                         f'Ваша основная эмоция сейчас: {TRANSLATE_DICT_FER.get(dominant_emotion, dominant_emotion)}',
+                         markup=types.ReplyKeyboardRemove())
         os.remove("photo.jpg")
-        found_film(message, bot, dominant_emotion)
-
-        ask_for_another_file(message, bot)
+        bot.send_message(message.chat.id, f'Хотите выбрать случайный фильм или по вашим предпочтениям?',
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, extends_found_film, bot,
+                                       TRANSLATE_DICT_FER.get(dominant_emotion, dominant_emotion))
 
 
     except Exception as e:
@@ -128,28 +146,101 @@ def process_photo_message(message, bot):
         bot.register_next_step_handler(message, process_photo_message, bot)
 
 
-def found_film(message, bot, emot):
-    response = requests.get(
-        API_LINK.format(
-            genres_name=GENRE_DICT[emot],
-            count=random.randint(1, 1000)
-        ),
-        headers={
-            'X-API-KEY': '132Z32C-5Y044XJ-H0DXA4A-M5JCFYH',
-        }
-    ).json()
-    if len(response['docs']) == 0:
-        found_film(message, bot, emot)
-    response = response['docs'][random.randint(0, len(response['docs']) - 1)]
-    if response.get('description') is not None and response.get('poster') != None and len(response['names']) > 1:
-        bot.edit_message_text(
-            f'<b>{response["names"][0]["name"]} ({response["names"][1]["name"]})</b>\n\n'
-            f'{response["description"]}',
-            chat_id=message.chat.id,
-            message_id=message.message_id,
-            parse_mode='HTML',
-        )
-        bot.send_photo(message.chat.id, response["poster"]["url"])
+def found_film(message, bot, emot, link=API_LINK):
+    message = bot.send_message(message.chat.id, f'Начинается поиск фильма для вас\nПодождите пару секунд')
+    global response
+    if ('countries' and 'year') in link:
+        response = requests.get(
+            API_LINK.format(
+                genres_name=GENRE_DICT[emot],
+                count=random.randint(1, 1000),
+                countriess=get_random_country_for_user(message.from_user.id),
+                years=get_random_year_for_user(message.from_user.id)
+            ),
+            headers={
+                'X-API-KEY': '132Z32C-5Y044XJ-H0DXA4A-M5JCFYH',
+            }
+        ).json()
+        print(link)
+        if len(response['docs']) == 0:
+            found_film(message, bot, emot)
+        response = response['docs'][random.randint(0, len(response['docs']) - 1)]
+        if response.get('description') is not None and response.get('poster') != None and len(response['names']) > 1:
+            bot.send_message(message.chat.id,
+                f'<b>{response["names"][0]["name"]} ({response["names"][1]["name"]})</b>\n\n'
+                f'{response["description"]}',
+                parse_mode='HTML',
+            )
+            bot.send_photo(message.chat.id, response["poster"]["url"])
+
 
     else:
+        response = requests.get(
+            API_LINK.format(
+                genres_name=GENRE_DICT[emot],
+                count=random.randint(1, 1000)
+            ),
+            headers={
+                'X-API-KEY': '132Z32C-5Y044XJ-H0DXA4A-M5JCFYH',
+            }
+        ).json()
+        if len(response['docs']) == 0:
+            found_film(message, bot, emot)
+        response = response['docs'][random.randint(0, len(response['docs']) - 1)]
+        if response.get('description') is not None and response.get('poster') != None and len(response['names']) > 1:
+            bot.send_message(message.chat.id,
+                f'<b>{response["names"][0]["name"]} ({response["names"][1]["name"]})</b>\n\n'
+                f'{response["description"]}',
+                             parse_mode='HTML',
+            )
+            bot.send_photo(message.chat.id, response["poster"]["url"])
+
+
+        else:
+            found_film(message, bot, emot)
+
+
+def extends_found_film(message, bot, emot):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Другой фильм')
+    item2 = types.KeyboardButton('Мне нравится фильм')
+    markup.add(item1, item2)
+    text = message.text
+
+    if (text == "Случайный фильм" or text == "Другой фильм") :
         found_film(message, bot, emot)
+        bot.send_message(message.chat.id, f'Выберите одну из опций ниже',
+                                   reply_markup=markup)
+        bot.register_next_step_handler(message, extends_found_film,*[bot, emot])
+    if text == 'Мне нравится фильм':
+        print(message.from_user.id)
+        add_user(message.from_user.id)
+        year = response['year']
+
+        country_names = [country['name'] for country in response['countries']]
+        for i in country_names:
+            add_country(message.from_user.id, i)
+        add_time_interval(message.from_user.id, year - year % 10, year + 9 - year % 10)
+        bot.send_message(message.chat.id, f'Фильм добавлен в ваши предпочтения,в следующий раз будут '
+                                                    f'искаться похожие фильмы',
+                                   reply_markup=types.ReplyKeyboardRemove())
+        #bot.register_next_step_handler(message, ask_for_another_file(message,bot))
+
+    if text == 'Мои предпочтения':
+
+        if find_user(message.from_user.id):
+            get_random_country_for_user(message.from_user.id)
+            found_film(message, bot, emot,link=API_LINK+'&year={yearss}&countries.name={countriess}')
+            message = bot.send_message(message.chat.id, f'Выберите одну из опций ниже',
+                                       reply_markup=markup)
+            bot.register_next_step_handler(message, extends_found_film, bot, emot)
+        else:
+            bot.send_message(message.chat.id, f'Вас еще нет в нашей базе,поэтому сначала нужно оценить любой фильм '
+                                              f'из тех что будут дальше')
+            message.text='Случайный фильм'
+            bot.register_next_step_handler(message, extends_found_film(message,bot,emot))
+
+
+
+
+
